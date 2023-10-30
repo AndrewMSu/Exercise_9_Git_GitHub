8c6d073355c5369e4e58ab74185a37460528e85d
Andrew Su <andrew.m.su02@gmail.com>
2023-10-29 20:09:19 -0400

    Added Name and Date to script

--- Exercise_9.py
+++ Exercise_9.py
@@ -1,3 +1,10 @@
+"""
+Name: Andrew Su 
+Directory ID: asu4530
+Date: 2023-10-29
+Assignment: Exercise 9: Git and GitHub
+"""
+
 """ A simple program that prints out hello if you give a name """
 def Hello(name):
     print(f"Hello {name}")
